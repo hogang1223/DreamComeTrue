bffe88d052408a0d8f8da02557cb3e48135663de
hogang <hyogangster1@gmail.com>
2023-08-01 22:10:00 +0900

    5일차: 옷가게 할인, 아이스아메리카노, 나이출력, 배열뒤집기

---
+++ Programmers/Programmers/LevelZero/Day05/ReversedArray.swift
@@ -0,0 +1,16 @@
+//
+//  ReversedArray.swift
+//  Programmers
+//
+//  Created by hogang on 2023/08/01.
+//
+
+import Foundation
+
+// 배열 뒤집기
+func reversedArray(_ num_list:[Int]) -> [Int] {
+    /*
+     정수가 들어 있는 배열 num_list가 매개변수로 주어집니다. num_list의 원소의 순서를 거꾸로 뒤집은 배열을 return하도록 solution 함수를 완성해주세요.
+     */
+    return num_list.reversed()
+}
