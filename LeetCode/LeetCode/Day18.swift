6662a803ea4e94b79528d368e27ba0bc7f204565
hogang <hyogangster1@gmail.com>
2023-09-19 19:52:09 +0900

    228. Summary Ranges

--- LeetCode/LeetCode/Day18.swift
+++ LeetCode/LeetCode/Day18.swift
@@ -7,6 +7,29 @@
 
 import Foundation
 
+// 228. Summary Ranges
+struct SummaryRanges {
+    func summaryRanges(_ nums: [Int]) -> [String] {
+        var result = [String]()
+        var stack = [Int]()
+        for (index, num) in nums.enumerated() {
+            if index != nums.count - 1 && nums[index+1] == num+1 {
+                stack.append(num)
+            }
+            else {
+                if stack.isEmpty {
+                    result.append(String(num))
+                }
+                else {
+                    result.append("\(stack.first!)->\(num)")
+                    stack.removeAll()
+                }
+            }
+        }
+        return result
+    }
+}
+
 // 704. Binary Search
 struct BinarySearch {
     /*
